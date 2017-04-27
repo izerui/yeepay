58ae021c790715bce8af6f8c455c5a50fa871587
serv <serv@liuyuhuadeMac-mini-2.local>
2017-04-27 10:21:04 +0800

    update

--- src/test/java/com/github/izerui/yeepay/PayTest.java
+++ src/test/java/com/github/izerui/yeepay/PayTest.java
@@ -64,9 +64,9 @@ public class PayTest {
 //        YeepayEngine engine = new YeepayEngine();
 
         RefundRequest request = new RefundRequest();
-//        request.setP2_Order("1222223");
+        request.setP2_Order("222");
         request.setP3_Amt("0.01");
-        request.setPb_TrxId("517283272066134I");
+        request.setPb_TrxId("868855800142162B");
         RefundResponse response = engine.refund(request);
         System.out.println(QueryFormUtils.getEncodedQueryParams(response).toString());
     }
